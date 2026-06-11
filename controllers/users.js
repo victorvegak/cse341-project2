dda163b959b38d59edcb926c4a94542bd3162b4b
VictorV <vvega28@outlook.com>
2026-06-10 21:42:12 -0600

    Project 2

---
+++ controllers/users.js
@@ -0,0 +1,24 @@
+const mongodb = require('../data/database');
+const objectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+    const result = await mongodb.getDatabase().db().collection('users').find();
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users);
+    }); 
+};
+
+const getSingle = async (req, res) => {
+    const userId = new objectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(0);
+    });    
+};
+
+module.exports = {
+    getAll,
+    getSingle
+};
